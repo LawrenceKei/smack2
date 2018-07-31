d86710e021e0082cff4954d3e55a4685288d66cd
Lawrence KEI <lawkei2002a@yahoo.com.hk>
2018-07-31 14:44:56 +1000

    Lession 4

---
+++ Smack2/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack2
+//
+//  Created by admin on 31/7/18.
+//  Copyright © 2018 keiit. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack2/Utilites/constants.swift
@@ -0,0 +1,14 @@
+//
+//  constants.swift
+//  Smack2
+//
+//  Created by admin on 31/7/18.
+//  Copyright © 2018 keiit. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
+
